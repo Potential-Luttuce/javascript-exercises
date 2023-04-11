0bdffa8bbfb1b351e3ec6e0bfd43d0bafc1c653a
Mike <Cuestamichael0@gmail.com>
2023-04-10 22:44:12 -0400

    Finished test 1 - enabled test 2

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let string = "";
+    let wordLength = word.length + 1;
+    for (let i = 1; i < wordLength; i++) {
+        string += word.at(-i);
+    }   
+    return string;
 };
 
 // Do not edit below this line
