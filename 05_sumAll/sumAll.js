25aad2135c56c5e6da12f72ea03c426a409e5c41
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-11 21:12:38 -0400

    passed test 2 (code for 3 already added in this commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
     for (let i = num1; i <= num2; i++) {
         sum += i;
     }
