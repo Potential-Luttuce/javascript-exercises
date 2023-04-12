866de5041471a869438c5c9ae3af8e69fdf2e163
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-11 20:52:39 -0400

    I had atypo but solution now works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
-const removeFromArray = function(array, ...args) {
+const removeFromArray = function(array, ...args) { //...rest operator
     const newArray = [];
     array.forEach((item) => {
-        if (!args.includes(item)) {
-            newArray.puch(item);
+        if (!args.includes(item)) { //grab any item in array that is NOT in the ...args
+            newArray.push(item);
         }
     })
 
-    return array;
+    return newArray;
 };
 
 // Do not edit below this line
