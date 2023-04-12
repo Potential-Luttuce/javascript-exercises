dcc31f504238cb2180e2957b6ef13d64a534968f
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-11 22:35:23 -0400

    Completed teest 1 for both functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  const celTemp = (tempF - 32) * (9/5);
+  return celTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  const fahTemp = (tempC * (9/5)) + 32;
+  return fahTemp;
 };
 
 // Do not edit below this line
