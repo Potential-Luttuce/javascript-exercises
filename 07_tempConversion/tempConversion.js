7fb64fe1e7fab4bc6190a79adcdd67ae2dc7aa80
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-11 22:46:06 -0400

    Completed test 2 for both functions - I had equestion for function 1 wrong

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(tempF) {
-  const celTemp = (tempF - 32) * (9/5);
-  return celTemp;
+  const celTemp = (tempF - 32) * (5/9);
+  return Math.round(celTemp * 10) / 10;
 };
 
 const convertToFahrenheit = function(tempC) {
   const fahTemp = (tempC * (9/5)) + 32;
-  return fahTemp;
+  return Math.round(fahTemp * 10) / 10;
 };
 
 // Do not edit below this line
