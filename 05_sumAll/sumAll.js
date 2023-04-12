88896d4850b1da26b89606f03f82542f7f21638c
Mike Cuesta <cuestamichael0@gmail.com>
2023-04-11 21:38:42 -0400

    Completed test 4 'negative parameter'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,6 +5,9 @@ const sumAll = function(num1, num2) {
         num1 = num2;
         num2 = temp;
     }
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
     for (let i = num1; i <= num2; i++) {
         sum += i;
     }
